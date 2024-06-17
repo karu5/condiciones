9b31181c6c5334bcf15a55f43133452246a3b008
karu5 <fabiolakmschmidt@gmail.com>
2024-06-17 00:26:40 -0400

    2do ejercicio mod 3

---
+++ ejerc-1/js/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+/*const imagen = document.querySelector('#imagen');
+
+if (document.querySelector('#imagen').style.border === '2px solid red') {
+    document.querySelector('#imagen').style.border = '';
+ } else {
+  document.querySelector('#imagen').style.border = '2px solid red';
+ } */
